108505732f40f3ee859287a09298ae856ec87347
rauljimenezz <rauljimenezgarcia2004@gmail.com>
2024-06-12 11:18:32 +0200

    1

---
+++ templates/trabajo/js/custom.js
@@ -0,0 +1,31 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    $(window).load(function(){
+      $('.preloader').fadeOut(1000); 
+    });
+
+    $('.custom-link').click(function(){
+    var el = $(this).attr('href');
+    var elWrapped = $(el);
+    var header_height = $('.navbar').height() + 10;
+
+    scrollToDiv(elWrapped,header_height);
+    return false;
+
+    function scrollToDiv(element,navheight){
+      var offset = element.offset();
+      var offsetTop = offset.top;
+      var totalScroll = offsetTop-navheight;
+
+      $('body,html').animate({
+      scrollTop: totalScroll
+      }, 300);
+  }
+});
+    
+  })(window.jQuery);
+
+
---
+++ templates/trabajo/js/magnific-popup-options.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  var magnifPopup = function() {
+    $('.popup-image').magnificPopup({
+      type: 'image',
+      removalDelay: 300,
+      mainClass: 'mfp-with-zoom',
+      gallery:{
+        enabled:true
+      },
+      zoom: {
+        enabled: true,
+
+        duration: 300,
+        easing: 'ease-in-out',
+
+        opener: function(openerElement) {
+        return openerElement.is('img') ? openerElement : openerElement.find('img');
+        }
+      }
+    });
+  };
+  magnifPopup();
+
+});
